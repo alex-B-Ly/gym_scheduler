fd14cff0215882c9bf5f781efb10ddecb6695501
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-05-01 17:13:14 -0400

    Created wildcard route that sends index.html by default.

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send('Welcome');
+router.get('*', function(req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
 });
 
 module.exports = router;
